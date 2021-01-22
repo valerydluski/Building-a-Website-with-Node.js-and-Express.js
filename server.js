7fdc3d44bf9d0dcd927b7bae8aa8eddc54184eae
Valery Dluski <valerydluski@gmail.com>
2021-01-22 21:39:07 +0300

    feat: Add a session management middleware

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const path = require('path');
+const cookieSession = require('cookie-session');
+
+const FeedbackService = require('./services/FeedbackService');
+const SpeakersService = require('./services/SpeakerService');
+
+const feedbackService = new FeedbackService('./data/feedback.json');
+const speakersService = new SpeakersService('./data/speakers.json');
 
 const routes = require('./routes');
 
@@ -7,12 +14,27 @@ const app = express();
 
 const port = 3000;
 
+app.set('trust proxy', 1);
+
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['ashdvhHGS712', 'jhashbjxcyg2'],
+  })
+);
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
 app.use(express.static(path.join(__dirname, './static')));
 
-app.use('/', routes());
+app.use(
+  '/',
+  routes({
+    feedbackService,
+    speakersService,
+  })
+);
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}!`);
