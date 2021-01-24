b06391b835e432b312b4ce69b975098d53f643d0
Valery Dluski <valerydluski@gmail.com>
2021-01-24 10:00:49 +0300

    feat: Template variables in more detail

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const routes = require('./routes');
 
 const app = express();
 
+app.locals.siteName = 'ROUX Academy';
+
 const port = 3000;
 
 app.set('trust proxy', 1);
@@ -19,14 +21,23 @@ app.set('trust proxy', 1);
 app.use(
   cookieSession({
     name: 'session',
-    keys: ['ashdvhHGS712', 'jhashbjxcyg2'],
+    keys: ['Ghdur687399s7w', 'hhjjdf89s866799'],
   })
 );
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
+app.locals.siteName = 'ROUX Meetups';
+
 app.use(express.static(path.join(__dirname, './static')));
+
+app.use(async (request, response, next) => {
+  const names = await speakersService.getNames();
+  response.locals.speakerNames = names;
+  console.log(response.locals);
+  return next();
+});
 
 app.use(
   '/',
