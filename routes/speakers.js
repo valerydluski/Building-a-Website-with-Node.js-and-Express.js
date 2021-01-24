c15128feb53ead91cafb4a3c1b78aed56531cf28
Valery Dluski <valerydluski@gmail.com>
2021-01-24 17:07:34 +0300

    feat: Solution: Partials and lists

--- routes/speakers.js
+++ routes/speakers.js
@@ -7,23 +7,18 @@ module.exports = (params) => {
 
   router.get('/', async (request, response) => {
     const speakers = await speakersService.getList();
-    const allArtworks = await speakersService.getAllArtwork();
-    response.render('layout', {
-      pageTitle: 'Speakers',
-      template: 'speakers',
-      speakers,
-      allArtworks,
-    });
+    const artwork = await speakersService.getAllArtwork();
+    response.render('layout', { pageTitle: 'Speakers', template: 'speakers', speakers, artwork });
   });
 
   router.get('/:shortname', async (request, response) => {
     const speaker = await speakersService.getSpeaker(request.params.shortname);
-    const artworkForSpeaker = await speakersService.getArtworkForSpeaker(request.params.shortname);
+    const artwork = await speakersService.getArtworkForSpeaker(request.params.shortname);
     response.render('layout', {
       pageTitle: 'Speakers',
       template: 'speakers-detail',
       speaker,
-      artworkForSpeaker,
+      artwork,
     });
   });
 
